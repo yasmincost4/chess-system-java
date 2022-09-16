79731c354741398db62fbca4af5b1d463a80cc3b
Yasmin Costa <yasmincost4@gmail.com>
2022-09-16 13:13:23 -0300

    Fixing UI error

--- src/application/UI.java
+++ src/application/UI.java
@@ -43,6 +43,16 @@ public class UI {
            throw new InputMismatchException("Error reading ChessPosition valid values are from a1 to h8");
        }
     }
+    public static void printBoard(ChessPiece[][] pieces) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces.length; j++) {
+                printPiece(pieces[i][j], false);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
         for(int i=0; i<pieces.length; i++){
             System.out.print((8 - i) + " ");
