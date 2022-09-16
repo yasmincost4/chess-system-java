441b25cdba7e38b87ad4db7a9f2a72971d94afcc
Yasmin Costa <yasmincost4@gmail.com>
2022-09-16 14:16:19 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import boardgame.Board;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -11,12 +10,14 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        while (true){
-            try{
+
+        while (true) {
+            try {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -28,13 +29,16 @@ public class Program {
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
-                ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             }
-            catch (ChessException | InputMismatchException e){
+            catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
         }
-
     }
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -32,17 +33,25 @@ public class UI {
         System.out.flush();
     }
 
-    public static ChessPosition readChessPosition(Scanner sc){
-       try {
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
             String s = sc.nextLine();
             char column = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
         }
-       catch (RuntimeException e){
-           throw new InputMismatchException("Error reading ChessPosition valid values are from a1 to h8");
-       }
+        catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+        }
     }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -53,21 +62,23 @@ public class UI {
         }
         System.out.println("  a b c d e f g h");
     }
-    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
-        for(int i=0; i<pieces.length; i++){
+
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
-            for(int j=0; j< pieces.length; j++){
+            for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
-    private static void printPiece(ChessPiece piece, boolean background){
-        if(background){
+
+    private static void printPiece(ChessPiece piece, boolean background) {
+        if (background) {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
-        if(piece == null){
+        if (piece == null) {
             System.out.print("-" + ANSI_RESET);
         }
         else {
@@ -80,6 +91,5 @@ public class UI {
         }
         System.out.print(" ");
     }
-
 
 }
