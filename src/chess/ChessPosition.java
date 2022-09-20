4ee7624c4e9424ef71fa96110933d18f8130ffdd
Yasmin Costa <yasmincost4@gmail.com>
2022-09-20 14:22:38 -0300

    Check Logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -7,8 +7,8 @@ public class ChessPosition {
     private int row;
 
     public ChessPosition(char column, int row) {
-        if(column < 'a' || column > 'h' || row < 1 || row > 8){
-            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
         }
         this.column = column;
         this.row = row;
@@ -22,12 +22,12 @@ public class ChessPosition {
         return row;
     }
 
-    protected Position toPosition(){
+    protected Position toPosition() {
         return new Position(8 - row, column - 'a');
     }
 
-    protected static ChessPosition fromPosition(Position position){
-        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
 
     @Override
